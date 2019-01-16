65383c4d842ff0597132082c361c3eccaee42a08
Andrew Cravenho <andrew@cravenho.me>
2019-01-16 13:30:59 -0500

    Bump Version

--- ledger/template/ethereum-on-azure/technology-samples/blockscout/install_blockscout.sh
+++ ledger/template/ethereum-on-azure/technology-samples/blockscout/install_blockscout.sh
@@ -56,7 +56,7 @@ sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with passw
 sudo apt-get install -y git
 sudo git clone https://github.com/poanetwork/blockscout.git && echo "cloned"
 cd blockscout
-sudo git checkout 869425df && echo "checked out"; cd -
+sudo git checkout 53ea60c3 && echo "checked out"; cd -
 sudo chmod -R a+x blockscout && echo "permissions granted"
 
 # Setup WebSocket Client
