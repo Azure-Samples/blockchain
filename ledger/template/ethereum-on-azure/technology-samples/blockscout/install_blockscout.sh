d52e4449a6ed2672722d444c75a885568e645ff9
Andrew Cravenho <andrew@cravenho.me>
2019-01-16 16:33:15 -0500

    Specify erlang version

--- ledger/template/ethereum-on-azure/technology-samples/blockscout/install_blockscout.sh
+++ ledger/template/ethereum-on-azure/technology-samples/blockscout/install_blockscout.sh
@@ -28,7 +28,7 @@ WEBSOCKET_PORT=${5:-8547}
 # Erlang VM & Elixir Install
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
-sudo apt-get -y install esl-erlang
+sudo apt-get -y install esl-erlang=1:21.1.1-1
 sudo apt-get -y install elixir=1.7.4-1
 
 # gcc, make and build-essential Install
