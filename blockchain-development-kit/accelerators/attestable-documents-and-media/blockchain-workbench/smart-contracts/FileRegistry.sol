d51f90d15ba6b4ade89dcf2c7ae2d1fdd0d76440
Chris Segura <chrisseg@microsoft.com>
2019-02-05 14:27:35 -0800

    fixed 2nd constructor and formatting fix

--- blockchain-development-kit/accelerators/attestable-documents-and-media/blockchain-workbench/smart-contracts/FileRegistry.sol
+++ blockchain-development-kit/accelerators/attestable-documents-and-media/blockchain-workbench/smart-contracts/FileRegistry.sol
@@ -223,127 +223,127 @@ contract FileRegistry {
 
 contract File {
 
-// Registry
-FileRegistry MyFileRegistry;
-address public RegistryAddress;
+	// Registry
+	FileRegistry MyFileRegistry;
+	address public RegistryAddress;
 
 
-//File Proeprties
-//Set of States
-enum StateType { Active, Deleted}
-StateType public  State;
-address public BlockchainAgent; // agent that processed the File
-string public FileId; //identifier for the File, stored off chain
-//address public Owner; //identifier for the Owner of the File, stored off chain
-string public Location; //The location of the file, e.g. URI
-string public FileHash; // text here, but could be an Id
-string public FileMetadataHash; // text here, but coudl be an Id
-string public ContentType; // text, represents the color of the File
-string public Etag; // text, represents the color of the File
-string public ProcessedDateTime; 
-string public DeletionRecordedDateTime;
+	//File Proeprties
+	//Set of States
+	enum StateType { Active, Deleted}
+	StateType public  State;
+	address public BlockchainAgent; // agent that processed the File
+	string public FileId; //identifier for the File, stored off chain
+	//address public Owner; //identifier for the Owner of the File, stored off chain
+	string public Location; //The location of the file, e.g. URI
+	string public FileHash; // text here, but could be an Id
+	string public FileMetadataHash; // text here, but coudl be an Id
+	string public ContentType; // text, represents the color of the File
+	string public Etag; // text, represents the color of the File
+	string public ProcessedDateTime; 
+	string public DeletionRecordedDateTime;
 
 
-function File (string registryAddress, string fileId, string location, string fileHash, string fileMetadataHash, string contentType, string etag, string processedDateTime) public {
-    FileId = fileId;
-    Location = location;
-    FileHash = fileHash;
-    FileMetadataHash = fileMetadataHash;
-    ContentType = contentType;
-    Etag = etag;
-    ProcessedDateTime = processedDateTime;
-    RegistryAddress = stringToAddress(registryAddress);
+	constructor (string registryAddress, string fileId, string location, string fileHash, string fileMetadataHash, string contentType, string etag, string processedDateTime) public {
+		FileId = fileId;
+		Location = location;
+		FileHash = fileHash;
+		FileMetadataHash = fileMetadataHash;
+		ContentType = contentType;
+		Etag = etag;
+		ProcessedDateTime = processedDateTime;
+		RegistryAddress = stringToAddress(registryAddress);
 
-    MyFileRegistry = FileRegistry(RegistryAddress);
+		MyFileRegistry = FileRegistry(RegistryAddress);
 
-   //If this file id is already registered, revert
-   if (MyFileRegistry.IsRegisteredFileId(stringToBytes32(FileId))) revert();
+	   //If this file id is already registered, revert
+	   if (MyFileRegistry.IsRegisteredFileId(stringToBytes32(FileId))) revert();
 
-    //NOTE - Can hardcode registry if previously registry previously deplyed to the chain to avoid having to call AssignRegistry;
-    MyFileRegistry.RegisterFile32(address(this), stringToBytes32(FileId));
- 
-    State = StateType.Active;
-    
-}
-
-
-function RegisterFile(address registryAddress) public {
-    
-    // only assign if there isn't one assigned already
-    if (RegistryAddress != 0x0) revert(); 
-    RegistryAddress = registryAddress;
-    
-    if (State != StateType.Active) revert();
-    
-    MyFileRegistry = FileRegistry(RegistryAddress);
-    
-    //Check to see if the File is already registered and 
-    //also check to see if this File can have its ownership transferred
-
-    if (MyFileRegistry.IsRegisteredFileContractAddress(address(this))) revert();
-    
-    
-
-    MyFileRegistry.RegisterFile32(address(this), stringToBytes32(FileId));
+		//NOTE - Can hardcode registry if previously registry previously deplyed to the chain to avoid having to call AssignRegistry;
+		MyFileRegistry.RegisterFile32(address(this), stringToBytes32(FileId));
+	 
+		State = StateType.Active;
+		
+	}
 
 
-}
+	function RegisterFile(address registryAddress) public {
+		
+		// only assign if there isn't one assigned already
+		if (RegistryAddress != 0x0) revert(); 
+		RegistryAddress = registryAddress;
+		
+		if (State != StateType.Active) revert();
+		
+		MyFileRegistry = FileRegistry(RegistryAddress);
+		
+		//Check to see if the File is already registered and 
+		//also check to see if this File can have its ownership transferred
 
- function  Delete(string deletionProcessedDateTime) public{
-    DeletionRecordedDateTime = deletionProcessedDateTime;
-    State = StateType.Deleted;
-}
+		if (MyFileRegistry.IsRegisteredFileContractAddress(address(this))) revert();
+		
+		
 
-//-----------------------------------------------------
-// Supporting Functions
-//-----------------------------------------------------
-    
-    function  stringToBytes32(string memory source) private returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
+		MyFileRegistry.RegisterFile32(address(this), stringToBytes32(FileId));
 
-        assembly {
-            result := mload(add(source, 32))
-        }
-    }
-    
-    function bytes32ToString (bytes32 x)  private returns (string) {
-        bytes memory bytesString = new bytes(32);
-        uint charCount = 0;
-        for (uint j  = 0; j < 32; j++) {
-            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-                }
-            }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (j = 0; j < charCount; j++) {
-            bytesStringTrimmed[j] = bytesString[j];
-            }
-        return string(bytesStringTrimmed);
-        
-    }
-    function compareStrings (string a, string b) view private returns (bool){
-        return keccak256(a) == keccak256(b);
-    }
-function stringToAddress(string _a) internal returns (address){
-     bytes memory tmp = bytes(_a);
-     uint160 iaddr = 0;
-     uint160 b1;
-     uint160 b2;
-     for (uint i=2; i<2+2*20; i+=2){
-         iaddr *= 256;
-         b1 = uint160(tmp[i]);
-         b2 = uint160(tmp[i+1]);
-         if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
-         else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
-         if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
-         else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
-         iaddr += (b1*16+b2);
-     }
-     return address(iaddr);
-    }
+
+	}
+
+	 function  Delete(string deletionProcessedDateTime) public{
+		DeletionRecordedDateTime = deletionProcessedDateTime;
+		State = StateType.Deleted;
+	}
+
+	//-----------------------------------------------------
+	// Supporting Functions
+	//-----------------------------------------------------
+		
+		function  stringToBytes32(string memory source) private returns (bytes32 result) {
+			bytes memory tempEmptyStringTest = bytes(source);
+			if (tempEmptyStringTest.length == 0) {
+				return 0x0;
+			}
+
+			assembly {
+				result := mload(add(source, 32))
+			}
+		}
+		
+		function bytes32ToString (bytes32 x)  private returns (string) {
+			bytes memory bytesString = new bytes(32);
+			uint charCount = 0;
+			for (uint j  = 0; j < 32; j++) {
+				byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+				if (char != 0) {
+					bytesString[charCount] = char;
+					charCount++;
+					}
+				}
+			bytes memory bytesStringTrimmed = new bytes(charCount);
+			for (j = 0; j < charCount; j++) {
+				bytesStringTrimmed[j] = bytesString[j];
+				}
+			return string(bytesStringTrimmed);
+			
+		}
+		function compareStrings (string a, string b) view private returns (bool){
+			return keccak256(a) == keccak256(b);
+		}
+	function stringToAddress(string _a) internal returns (address){
+		 bytes memory tmp = bytes(_a);
+		 uint160 iaddr = 0;
+		 uint160 b1;
+		 uint160 b2;
+		 for (uint i=2; i<2+2*20; i+=2){
+			 iaddr *= 256;
+			 b1 = uint160(tmp[i]);
+			 b2 = uint160(tmp[i+1]);
+			 if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
+			 else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
+			 if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
+			 else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
+			 iaddr += (b1*16+b2);
+		 }
+		 return address(iaddr);
+	}
 }
